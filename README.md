55c56c676e55a388ed8df8ab5479b81e55a48c1b
Evgen <evgen.murashko@gmail.com>
2016-09-27 12:15:48 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# homebridge-homeds-http-garagedoor
+
+```js
+{
+    "accessory": "HomeDSHttpGarageDoor",
+    "name": "Гараж",
+    "stateUrl": "http://localhost:3000/stateDoor",
+    "openUrl": "http://localhost:3000/openDoor",
+    "closeUrl": "http://localhost:3000/closeDoor",
+    "poolingInterval": 2000
+}
+```
---
+++ index.js
@@ -0,0 +1,108 @@
+var request = require("request");
+var fs = require("fs");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    On = homebridge.hap.Characteristic.On;
+
+    homebridge.registerAccessory("homebridge-homeds-http-lightbulb", "HomeDSHttpLightbulb", HomeDSAccessory);
+}
+
+function HomeDSAccessory(log, config) {
+    this.log = log;
+    this.name = config["name"];
+    this.stateUrl = config["stateUrl"];
+    this.onUrl = config["onUrl"];
+    this.offUrl = config["offUrl"];
+
+    this.poolingInterval = config["poolingInterval"];
+
+    this.lightservice = new Service.Lightbulb(this.name, this.name);
+
+    this.On = this.lightservice.getCharacteristic(On);
+
+    this.lightservice
+        .getCharacteristic(Characteristic.On)
+        .on('get', this.getState.bind(this))
+        .on('set', this.setState.bind(this));
+
+    this.init();
+
+}
+
+HomeDSAccessory.prototype = {
+
+    init: function() {
+        setTimeout(this.monitorState.bind(this), 10000);
+    },
+    monitorState: function() {
+
+        request.get({
+            url: this.stateUrl
+        }, function(err, response, body) {
+            if (!err && response.statusCode == 200) {
+                var state = 1;
+
+                if (body == 'false') {
+                    state = 0;
+                }
+
+                if (state != this.On.value) {
+                    this.On.setValue(state);
+                }
+
+            } else {
+                this.log("Server error");
+            }
+        }.bind(this));
+
+        setTimeout(this.monitorState.bind(this), this.poolingInterval);
+
+    },
+    getState: function(callback) {
+
+
+        this.log("Getting current state...");
+        request.get({
+            url: this.stateUrl
+        }, function(err, response, body) {
+            if (!err && response.statusCode == 200) {
+                var state = 1;
+
+                if (body == 'false')
+                    state = 0;
+
+                callback(null, state);
+
+            } else {
+                this.log("Error server set state");
+                callback(null);
+            }
+        }.bind(this));
+
+    },
+    setState: function(state, callback) {
+
+        this.log('Set lamp state to: ' + state);
+
+        request.get({
+            url: (state == 1) ? this.openUrl : this.closeUrl
+        }, function(err, response, body) {
+            if (!err && response.statusCode == 200) {
+
+                callback(null);
+            } else {
+                this.log("Error server set state");
+                callback(null);
+            }
+        }.bind(this));
+
+    }
+
+}
+
+HomeDSAccessory.prototype.getServices = function() {
+    return [this.lightservice];
+}
